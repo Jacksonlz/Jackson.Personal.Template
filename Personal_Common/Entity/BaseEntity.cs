30f711830e59e9a3d1375eab52e25e4ed3b9a457
Jackson.Liu <jacksonlz@163.com>
2023-06-12 16:47:26 +0800

    #add base repository

---
+++ Personal_Common/Entity/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Personal.Common.Entity
+{
+    public abstract record BaseEntity
+    {
+        [JsonIgnore]
+        public bool IsDeleted { get; set; }
+        [JsonIgnore]
+        public DateTime CreateTime { get; set; } = DateTime.UtcNow;
+        [JsonIgnore]
+        public DateTime? UpdatedTime { get; set; }
+        public string? CreateBy { get; set; }
+        public string? UpdatedBy { get; set; }
+    }
+}
---
+++ Personal_Repository/Repository/BaseRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Personal.Common.Entity;
+using Personal.Repository.Share;
+
+namespace Personal.Repository.Repository;
+
+public abstract class BaseRepository<TEntity> where TEntity : BaseEntity
+{
+    protected readonly AppDbContext appDbContext;
+
+    public BaseRepository(AppDbContext appDbContext)
+    {
+        this.appDbContext = appDbContext;
+    }
+
+    public virtual async Task<TEntity> AddAsync(TEntity entity, string userId)
+    {
+        entity.CreateBy = userId;
+        entity.CreateTime = DateTime.Now;
+        entity.UpdatedBy = userId;
+        entity.UpdatedTime = DateTime.Now;
+        var result = await appDbContext.AddAsync<TEntity>(entity);
+        await appDbContext.SaveChangesAsync();
+        return result.Entity;
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> AddAsync(IEnumerable<TEntity> entities, string userId)
+    {
+        if (entities == null || !entities.Any())
+        {
+            return entities;
+        }
+        entities = entities.Select(e =>
+        {
+            e.CreateBy = userId;
+            e.CreateTime = DateTime.Now;
+            e.UpdatedBy = userId;
+            e.UpdatedTime = DateTime.Now;
+            return e;
+        });
+        await appDbContext.AddRangeAsync(entities);
+        await appDbContext.SaveChangesAsync();
+        return entities;
+    }
+
+    public virtual async Task<TEntity> UpdateAsync(TEntity entity, string userId)
+    {
+        entity.UpdatedBy = userId;
+        entity.UpdatedTime = DateTime.Now;
+        var entry = appDbContext.Entry<TEntity>(entity);
+        if (entry.State == EntityState.Detached)
+        {
+            appDbContext.Attach<TEntity>(entity);
+        }
+        var result = appDbContext.Update<TEntity>(entity);
+        foreach (var p in result.Properties)
+        {
+            if (!p.Metadata.IsPrimaryKey() && !p.IsModified)
+            {
+                p.IsModified = true;
+            }
+        }
+        await appDbContext.SaveChangesAsync();
+        return result.Entity;
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetListAsync(Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null)
+    {
+        IQueryable<TEntity> query = appDbContext.Set<T>().AsNoTracking().Where(e => !e.IsDeleted);
+        if (orderBy != null)
+        {
+            query = orderBy(query);
+        }
+        return await query.ToListAsync();
+    }
+}
